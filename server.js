277ab7824ae957747faf9cf888bf61618fac0277
Bears <evanbero1980@gmail.com>
2021-06-03 16:19:29 -0500

    updated package.json

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
 const cors = require('cors');
-
+const routes = require('./routes');
+const path = require('path');
 const app = express();
+require('dotenv').config();
 
 // Middleware
 app.use(express.json({ extended: false }));
 
-require('dotenv').config();
-const PORT = process.env.PORT || 3001;
-
-// Serve up static assets (usually on heroku)
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
 }
 
-// Express serve up index.html file if it doesn't recognize route
-const path = require('path');
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
@@ -27,7 +23,9 @@ app.use(cors());
 // Add routes, both API and View
 app.use(routes);
 
+const PORT = process.env.PORT || 3001;
+
 // Start the API server
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log(`🌎  ==> evanDev Server now listening on PORT ${PORT}!`);
 });
