165d4f205770a979b339a537173f4467ce33c720
Bears <evanbero1980@gmail.com>
2021-06-14 11:08:03 -0500

    testing sendgrid web api

--- routes/email-route.js
+++ routes/email-route.js
@@ -1,19 +1,9 @@
 require('dotenv').config();
 const router = require('express').Router();
-const nodemailer = require('nodemailer');
-const sgTransport = require('nodemailer-sendgrid-transport');
-// SendGrid
-// const sgMail = require('@sendgrid/mail');
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const options = {
-  service: 'SendGrid',
-  auth: {
-    api_user: process.env.USERNAME,
-    api_key: process.env.SENDGRID_API_KEY
-  }
-};
-const client = nodemailer.createTransport(sgTransport(options));
+// SendGrid
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Mail POST request
 router.post('/send', async (req, res) => {
@@ -31,34 +21,62 @@ router.post('/send', async (req, res) => {
       subject: ${subject} \n
       text: ${text} `;
 
-  // Contact email object
-  const mailObject = {
+  // Email object
+  const msg = {
     from: 'evanbero@evandev.com',
     to: 'evanbero@evandev.com',
     subject: subject,
-    text: content
+    text: content,
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>'
   };
 
-  console.log('this is the mail object sent:');
-  console.log(mailObject);
-
   // Function to SEND EMAIL to SendGrid
-  await client
-    .sendMail(mailObject)
+  await sgMail
+    .send(msg)
     .then(() => {
+      console.log('Email sent');
+      console.log(msg);
       res.json({
         status: 'success',
-        data: mailObject
+        data: msg
       });
-      console.log('Message sent: ' + info.res);
     })
     .catch(err => {
+      console.log(err);
       res.json({
         status: 'fail',
         error: err
       });
-      console.log('status:fail -- error: ' + err);
     });
 });
 
 module.exports = router;
+
+// const nodemailer = require('nodemailer');
+// const sgTransport = require('nodemailer-sendgrid-transport');
+
+// const options = {
+//   service: 'SendGrid',
+//   auth: {
+//     api_user: process.env.USERNAME,
+//     api_key: process.env.SENDGRID_API_KEY
+//   }
+// };
+// const client = nodemailer.createTransport(sgTransport(options));
+
+// await client
+//   .sendMail(msg)
+//   .then(() => {
+//     res.json({
+//       status: 'success',
+//       data: msg
+//     });
+//     console.log('Message sent: ' + info.res);
+//   })
+//   .catch(err => {
+//     res.json({
+//       status: 'fail',
+//       error: err
+//     });
+//     console.log('status:fail -- error: ' + err);
+//   });
