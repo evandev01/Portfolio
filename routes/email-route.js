4fdfa0386a501b8a3c0c3388cf2325b7acf43999
Bears <evanbero1980@gmail.com>
2021-06-28 15:15:51 -0500

    Telnet smtp relay test successful, begun integration and email test from site

--- routes/email-route.js
+++ routes/email-route.js
@@ -1,61 +1,61 @@
-require('dotenv').config();
-const router = require('express').Router();
+require('dotenv').config()
+const router = require('express').Router()
 
-const nodemailer = require('nodemailer');
-const sgTransport = require('nodemailer-sendgrid-transport');
+const nodemailer = require('nodemailer')
+const sgTransport = require('nodemailer-sendgrid-transport')
 const options = {
   host: process.env.SENDGRID_HOST,
   port: process.env.SENDGRID_PORT,
   auth: {
-    api_user: process.env.SENDGRID_USERNAME,
-    api_key: process.env.SENDGRID_PASSWORD
-  }
-};
-const client = nodemailer.createTransport(sgTransport(options));
+    api_user: process.env.USER,
+    api_key: process.env.PASS,
+  },
+}
+const client = nodemailer.createTransport(sgTransport(options))
 
 // Single Send Email POST request
 router.post('/send', async (req, res) => {
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const senderEmail = req.body.senderEmail;
-  const occupation = req.body.occupation;
-  const subject = req.body.subject;
-  const text = req.body.text;
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const senderEmail = req.body.senderEmail
+  const occupation = req.body.occupation
+  const subject = req.body.subject
+  const text = req.body.text
   const content = `
       firstName: ${firstName} \n 
       lastName: ${lastName} \n
       email: ${senderEmail} \n 
       occupation: ${occupation} \n
       subject: ${subject} \n
-      text: ${text} `;
+      text: ${text} `
 
   // Email object
   const email = {
-    from: 'evanbero@evandev.com',
-    to: 'evanbero@evandev.com',
+    from: `<evanbero@evandev.com>`,
+    to: `<evanbero@evandev.com>`,
     subject: subject,
     text: content,
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>'
-  };
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
 
   await client.sendMail(email, (err, info) => {
     if (err) {
-      console.log(error);
+      console.log(error)
       res.json({
         status: 'fail',
-        error: err
-      });
+        error: err,
+      })
     } else {
-      console.log('Message sent: ' + info.response);
+      console.log('Message sent: ' + info.response)
       res.json({
         status: 'success',
-        data: msg
-      });
+        data: msg,
+      })
     }
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
 
 //https://api.sendgrid.com/v3/mail/send
 
